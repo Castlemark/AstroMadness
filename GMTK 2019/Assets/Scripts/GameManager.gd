7d83245bcfcefbdb03e8a386e7be3b3ba2f26fbe
Marc Castells i Güell <marc.castells.guell@gmail.com>
2019-08-04 01:03:59 +0200

    Added menu and level transition

--- GMTK 2019/Assets/Scripts/GameManager.gd
+++ GMTK 2019/Assets/Scripts/GameManager.gd
@@ -1,9 +1,35 @@
 extends Node2D
 
+var level_idx : int
+
+func _ready() -> void:
+	level_idx = 0
+	
+	OS.window_size = Vector2( 720, 720)
+	OS.window_position -= OS.window_size/2
+
 func reset_level():
 	Physics2DServer.area_set_param(get_world_2d().space, Physics2DServer.AREA_PARAM_GRAVITY_VECTOR, Vector2.DOWN)
 	var current_scene = get_tree().get_current_scene().filename
 	get_tree().change_scene(current_scene)
 
 func go_to_next_level():
-	print("goint to next level")
+	call_deferred("_deferred_go_to_next_level")
+
+func _deferred_go_to_next_level():
+	var root = get_tree().get_root()
+	var current_scene : Node = root.get_child(root.get_child_count() - 1)
+	
+	level_idx += 1
+	var scene : Resource = load("res://Assets/Scenes/Levels/Level_" + str(level_idx) + ".tscn")
+	
+	if scene == null:
+		print("Go to end screen")
+	else:
+		print("going to level " + str(level_idx))
+		
+		current_scene.free()
+		current_scene = scene.instance()
+		
+		get_tree().get_root().add_child(current_scene)
+		get_tree().set_current_scene(current_scene)
